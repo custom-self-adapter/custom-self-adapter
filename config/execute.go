0502b4eae3185accb1be3d60a46c4363b9b0bf61
Humberto Fraga <humbertofraga@gmail.com>
2025-07-07 18:19:22 -0300

    Adapt CPA config package to CSA

---
+++ config/execute.go
@@ -0,0 +1,45 @@
+/*
+Copyright 2025 The Custom Self-Adapter Developers.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+// Methos describes a method for passing data/triggering logic, such as through
+// a shell command
+type Method struct {
+	Type    string `json:"type"`
+	Timeout int    `json:"timeout"`
+	Shell   *Shell `json:"shell"`
+	HTTP    *HTTP  `json:"http"`
+}
+
+// Shell describes configuration options for a shell command method
+type Shell struct {
+	Command    []string `json:"command"`
+	Entrypoint string   `json:"entrypoint"`
+	LogStderr  *bool    `json:"logStderr,omitempty"`
+}
+
+// HTTP describes configuration options for an HTTP request method
+type HTTP struct {
+	Method        string            `json:"method"`
+	URL           string            `json:"url"`
+	Headers       map[string]string `json:"headers,omitempty"`
+	SuccessCodes  []int             `json:"successCodes"`
+	ParameterMode string            `json:"parameterMode"`
+	CACert        *string           `json:"caCert"`
+	ClientCert    *string           `json:"clientCert"`
+	ClientKey     *string           `json:"clientKey"`
+}
---
+++ config/k8smetric.go
@@ -0,0 +1,45 @@
+/*
+Copyright 2025 The Custom Self-Adapter Developers.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	autoscalingv2 "k8s.io/api/autoscaling/v2"
+)
+
+// K8sMetricSpec defines which metrics to query from the metrics server
+type K8sMetricSpec autoscalingv2.MetricSpec
+
+// K8sMetricTarget defines the type of metric gathering, either target value, average value, or average utilization of a
+// specific metric
+type K8sMetricTarget autoscalingv2.MetricTarget
+
+// K8sObjectMetricSource defines gathering metrics for a kubernetes object (for example, hits-per-second on an Ingress
+// object).
+type K8sObjectMetricSource autoscalingv2.ObjectMetricSource
+
+// K8sPodsMetricSource defines gathering metrics describing each pod in the current scale target (for example,
+// transactions-processed-per-second).
+type K8sPodsMetricSource autoscalingv2.PodsMetricSource
+
+// K8sResourceMetricSource defines gathering metrics for a resource metric known to Kubernetes, as specified in requests
+// and limits, describing each pod in the current scale target (e.g. CPU or memory). Such metrics are built in to
+// Kubernetes.
+type K8sResourceMetricSource autoscalingv2.ResourceMetricSource
+
+// K8sExternalMetricSource defines gathering metrics for a metric not associated with any Kubernetes object (for example
+// length of queue in cloud messaging service, or QPS from loadbalancer running outside of cluster).
+type K8sExternalMetricSource autoscalingv2.ExternalMetricSource
